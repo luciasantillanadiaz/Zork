849cf62fdd5ba450087abfefec8a566502269f1c
Lucía Santillana Díaz <luciasantillanadiaz@gmail.com>
2026-01-19 21:20:36 +0100

    Integrate World and Command systems into the main game loop

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@
 #include <iterator>
 #include <algorithm>
 #include <cctype>
+#include "world.h"
+#include "command.h"
 
 using namespace std;
+
+const string BOLD = "\033[1m";
+const string RESET = "\033[0m";
+const string RED = "\033[31m";
 
 vector<string> parseInput(string& input) {
     transform(input.begin(), input.end(), input.begin(), ::tolower);
@@ -23,19 +29,32 @@ int main() {
     string input;
     vector<string> commands;
 
-    cout << "Welcome to Zork!\n" << endl;
+    World world;
+    Command* commandSystem = world.GetCommmandSystem();
+
+    cout << RED << BOLD << " --- WELCOME TO MY ZORK! ---\n" << RESET;
+
+    commands.push_back("look");
 
     while (1) {
-        cout << "> ";
-
-        getline(cin, input);
-        if (input.empty()) continue;       
-
-        commands = parseInput(input);
         if (commands.empty()) continue;
 
         if (commands[0] == "quit") break;
-        else cout << "Command not recognized." << endl;
+
+        string action = commands[0];
+        vector<string> args;
+        if (commands.size() > 1) {
+            args.assign(commands.begin() + 1, commands.end());
+        }
+
+        commandSystem->ExecuteCommand(action, args);    
+
+        cout << "> ";
+
+        getline(cin, input);
+        if (input.empty()) continue;
+
+        commands = parseInput(input);
     }
 
     cout << "\nThanks for playing!\n";
